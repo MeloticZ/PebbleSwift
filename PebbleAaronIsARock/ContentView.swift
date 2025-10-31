b9daecfbc688cd174c23b1d50bc1bf938c80594b
Korgo <hello@korgo.net>
2025-10-30 17:04:53 -0700

    Initial Commit

---
+++ PebbleAaronIsARock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PebbleAaronIsARock
+//
+//  Created by Korgo on 10/30/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PebbleAaronIsARock/PebbleAaronIsARockApp.swift
@@ -0,0 +1,17 @@
+//
+//  PebbleAaronIsARockApp.swift
+//  PebbleAaronIsARock
+//
+//  Created by Korgo on 10/30/25.
+//
+
+import SwiftUI
+
+@main
+struct PebbleAaronIsARockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
